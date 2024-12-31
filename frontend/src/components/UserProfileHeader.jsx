03f0b9e98c0f7b3f41b3fc0141eb03fcf1bf4aaa
anupamamaheepala <127319824+anupamamaheepala@users.noreply.github.com>
2024-12-31 09:01:36 +0530

    anu_31_1

--- frontend/src/components/UserProfileHeader.jsx
+++ frontend/src/components/UserProfileHeader.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import '../css/header.css';
 import logo from '../images/SLTlogo.jpg'
 import profileImg from "../images/profileImg.png";
@@ -6,6 +7,23 @@ import  Link from "antd/es/typography/Link";
 <img src={profileImg} alt="Logo" />;
 
 const UserProfileHeader = () => {
+
+  const [firstName, setFirstName] = useState("");
+
+  useEffect(() => {
+    const userId = localStorage.getItem("user_id");
+    if (userId) {
+      axios
+        .get(`http://localhost:5000/auth/get-user/${userId}`)
+        .then((response) => {
+          setFirstName(response.data.user.first_name);
+        })
+        .catch((error) => {
+          console.error("Error fetching user:", error);
+        });
+    }
+  }, []);
+
   return (
     <header className="header">
       <div className="logo">
@@ -19,7 +37,7 @@ const UserProfileHeader = () => {
           <li><a href="/contact">Contact Us</a></li>
         </ul>
       </nav>
-      <div className="auth-buttons">
+      <div className="auth-buttons-pic">
         <Link href="/userProfile">
         <img
                     alt="profileImg"
@@ -35,7 +53,9 @@ const UserProfileHeader = () => {
         </Link>
       
       </div>
-      
+      {firstName && (
+          <span className="user-firstname">{firstName}</span>
+        )}
     </header>
     
   );
