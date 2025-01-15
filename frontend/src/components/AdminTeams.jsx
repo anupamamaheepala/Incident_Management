9429ea6395c0315d17a952e2237f187dbbbba10a
anupamamaheepala <127319824+anupamamaheepala@users.noreply.github.com>
2025-01-15 21:23:40 +0530

    anu_25115_2

--- frontend/src/components/AdminTeams.jsx
+++ frontend/src/components/AdminTeams.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { Table, Tag, Tooltip, Input } from "antd";
+import axios from "axios";
+import "../css/admin.css";
 
 function AdminTeams() {
-  return (
-    <div className="container">
-      <h2>Teams</h2>
-    </div>
-  )
+    return (
+        <div className="admin-teams-container">
+            <div className="bg-image">
+            <div className="new-header">
+              <h5>All Teams</h5>
+            </div>
+            </div>
+        </div>
+      );
 }
 
 export default AdminTeams
