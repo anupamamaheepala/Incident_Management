523a9ad739da1c8a0bc0f7c9ee2f1b5ed1af2afb
anupamamaheepala <127319824+anupamamaheepala@users.noreply.github.com>
2025-01-16 09:07:38 +0530

    anu_25116_1

--- frontend/src/components/AdminFeedbacks.jsx
+++ frontend/src/components/AdminFeedbacks.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { Table, Tag, Tooltip, Input } from "antd";
+import axios from "axios";
+import "../css/admin.css";
 
 function AdminFeedbacks() {
   return (
-    <div className="container">
-      <h2>feedbacks</h2>
+    <div className="admin-teams-container">
+        <div className="bg-image">
+        <div className="new-header">
+          <h5>All Feedbacks</h5>
+        </div>
+        </div>
     </div>
-  )
+  );
 }
 
 export default AdminFeedbacks
