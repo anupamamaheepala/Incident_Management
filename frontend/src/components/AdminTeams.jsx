32fd20cbc1d0ed03c20a4c63f726c8d6905504b2
anupamamaheepala <127319824+anupamamaheepala@users.noreply.github.com>
2025-01-19 18:01:22 +0530

    anu_25119_2

--- frontend/src/components/AdminTeams.jsx
+++ frontend/src/components/AdminTeams.jsx
@@ -4,40 +4,6 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import "../css/admin.css";
 
-// const incidentTypes = [
-//   { title: "Billing and Payment Issues", description: "Handle billing discrepancies and payment concerns." },
-//   { title: "Service Interruptions", description: "Manage internet, call drops, and connectivity issues." },
-//   { title: "SIM Issues", description: "Assist with SIM activation, deactivation, and replacements." },
-//   { title: "Service Requests", description: "Handle requests for new connections, upgrades, and more." },
-//   { title: "Customer Portal Issues", description: "Resolve issues with login, features, and app functionalities." },
-//   { title: "Device Related Issues", description: "Support for faulty devices and router issues." },
-// ];
-
-// function AdminTeams() {
-//     return (
-//         <div className="admin-teams-container">
-//             <div className="bg-image">
-//             <div className="new-header">
-//               <h5>All Teams</h5>
-//             </div>
-//             <div className="admin-cards-container">
-//               <Row gutter={[30, 30]}>
-//                 {incidentTypes.map((type, index) => (
-//                   <Col xs={24} sm={12} md={12} key={index}>
-//                     <Card title={type.title} bordered={true} hoverable>
-//                       <p>{type.description}</p>
-//                     </Card>
-//                   </Col>
-//                 ))}
-//               </Row>
-//             </div>
-//             </div>
-//         </div>
-//       );
-// }
-
-// export default AdminTeams
-
 const incidentTypes = [
   { title: "Billing and Payment Issues", type: "Billing and payment" },
   { title: "Service Interruptions", type: "Service interruption" },
